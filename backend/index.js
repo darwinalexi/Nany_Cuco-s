2fd4358cb3985c348219246b5cfceba6824d013d
darwinalexisguerrerobaos <darwinalexisguerrerobaos@gmail.com>
2025-05-18 18:13:11 -0500

    se suben ultimos cambios en el backend

--- backend/index.js
+++ backend/index.js
@@ -8,46 +8,35 @@ import { validarToken } from "./src/controller/atutentication.js";
 import {router_client }from"./src/routes/router_client.js"
 import {router_order  }from"./src/routes/router.orders.js";
 import { router_venta } from "./src/routes/router.sales.js";
-
-
 const server = express();
 
-// Obtener __dirname correctamente en ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Usar puerto dinámico si está en Railway o 3333 localmente
 const port = process.env.PORT || 3333;
 
-// Middleware
-server.use(cors());
-server.use(body_parser.json());
-server.use(body_parser.urlencoded({ extended: false }));
-server.use(express.static("public"));
+server.get('/', (req, res) =>{
+    res.send('Hola mundo')
+})
 
-// Configuración del motor de vistas
-server.set("view engine", "ejs");
-server.set("views", path.join(__dirname, "view"));
+server.use(express.static('public'));
 
-// Ruta raíz
-server.get("/", (req, res) => {
-  res.send("Hola mundo");
-});
+server.use(cors())
+server.use(body_parser.json())
+server.use(body_parser.urlencoded({extended:false}))
 
-// Ruta de vista EJS
+//configuracion de rutas
+server.use(rutaaut)
+server.use( productos)
+server.use( router_of_user)
+server.use(router_order)
+server.use(router_client)
+server.use(router_venta)
+
+server.listen(port, ()=>{
+    console.log("servidor corriendo en el puerto "+port)
+})
+
+
+server.set("view  engine", "ejs")
+server.set("views", "./view/")
 server.get("/document", (req, res) => {
-  res.render("document.ejs");
-});
-
-// Rutas de la API
-server.use(rutaaut);
-server.use(productos);
-server.use(router_of_user);
-server.use(router_order);
-server.use(router_client);
-server.use(router_venta);
-
-// Iniciar servidor
-server.listen(port, () => {
-  console.log("Servidor corriendo en el puerto " + port);
-});
+  res.render("document.ejs")
+})
