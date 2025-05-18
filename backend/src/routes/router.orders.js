91893c9e5dfa1c890dfc80d06ea14f2dd8813610
darwinalexisguerrerobaos <darwinalexisguerrerobaos@gmail.com>
2025-05-18 17:46:32 -0500

    se suben cambios en el backend

--- backend/src/routes/router.orders.js
+++ backend/src/routes/router.orders.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {search, create_orders, show_orders, update_orders, count_orders, count_orders_sould, delete_orders, sold_value  } from "../controller/controller.orders.js";
 export const router_order= Router();
 
+
 router_order.post("/pedidos", create_orders)
 router_order.get("/pedidos", show_orders)
 router_order.get("/pedidos/:id", search)
