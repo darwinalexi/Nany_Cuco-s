d141b5094838b64143874a17fba7c8cfb6bd8403
darwinalexisguerrerobaos <darwinalexisguerrerobaos@gmail.com>
2025-05-19 10:37:03 -0500

    se eliminio rutass

--- backend/index.js
+++ backend/index.js
@@ -44,3 +44,7 @@ server.get("/document", (req, res) => {
 server.get('/', (req, res) => {
   res.status(200).send('Server is healthy');
 });
+
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en el puerto ${port}`);
+});
