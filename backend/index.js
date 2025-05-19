f5985b30371ffa042507bfc750195003f8b87d59
darwinalexisguerrerobaos <darwinalexisguerrerobaos@gmail.com>
2025-05-19 10:42:04 -0500

    se cambio el server del index

--- backend/index.js
+++ backend/index.js
@@ -1,45 +1,8 @@
 import express from "express";
-import body_parser from 'body-parser'
-import cors from'cors';
-{/*import { rutaaut } from "./src/routes/autentication.js";
-import productos from './src/routes/productos_routes.js'
-import { router_of_user } from "./src/routes/user.router.js";
-import { validarToken } from "./src/controller/atutentication.js";
-import {router_client }from"./src/routes/router_client.js"
-import {router_order  }from"./src/routes/router.orders.js";
-import { router_venta } from "./src/routes/router.sales.js";*/}
-
 
 const server = express();
 
 const port = process.env.PORT || 3333;
-
-
-//server.use(express.static('public'));
-
-server.use(cors())
-server.use(body_parser.json())
-server.use(body_parser.urlencoded({extended:false}))
-{/*
-//configuracion de rutas
-server.use(rutaaut)
-server.use( productos)
-server.use( router_of_user)
-server.use(router_order)
-server.use(router_client)
-server.use(router_venta)
-
-
-server.listen(port, '0.0.0.0', () => {
-    console.log(`Servidor corriendo en el puerto ${port}`);
-});
-
-
-server.set("view  engine", "ejs")
-server.set("views", "./view/")
-server.get("/document", (req, res) => {
-  res.render("document.ejs")
-}) */} 
 
 server.get('/', (req, res) => {
   res.status(200).send('Server is healthy');
