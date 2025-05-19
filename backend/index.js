4d291dcb256c04cf4a8ae0c426fc4d14d26181ba
darwinalexisguerrerobaos <darwinalexisguerrerobaos@gmail.com>
2025-05-19 10:50:20 -0500

    se cambio el server del index

--- backend/index.js
+++ backend/index.js
@@ -41,10 +41,6 @@ server.get("/document", (req, res) => {
   res.render("document.ejs")
 }) 
 
-server.get('/', (req, res) => {
-  res.status(200).send('Server is healthy');
-});
-
 server.listen(port, '0.0.0.0', () => {
   console.log(`Servidor corriendo en el puerto ${port}`);
 });
